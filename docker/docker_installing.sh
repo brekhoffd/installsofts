d576d271764623824ae9e6f892e78b0b8654fef1
Denys Brekhov <denys.brekhov@gmail.com>
2024-12-10 21:55:06 +0200

    Update docker_installing.sh

--- docker/docker_installing.sh
+++ docker/docker_installing.sh
@@ -34,7 +34,7 @@ update-locale LANG=en_US.UTF-8
 apt update && apt full-upgrade -y
 
 # Add Docker's official GPG key:
-apt install ca-certificates curl
+apt install ca-certificates curl -y
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -63,6 +63,6 @@ else
 	echo
 fi
 
-# Status of the Jenkins service:
+# Status of the Docker service:
 systemctl --no-pager status docker
 echo
